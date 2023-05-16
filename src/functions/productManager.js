9622e97232bade824da6836baed84e120d5138f8
ini <ini@ini>
2023-05-15 22:34:02 -0300

    Add - Thumbnails and validations to the product fields

--- src/functions/productManager.js
+++ src/functions/productManager.js
@@ -4,6 +4,7 @@ export default class ProductManager {
   constructor(fileName) {
     this.fileName = fileName;
     this.products = [];
+    this.thumbnailsPath = "public/";
     this.loadProducts();
   }
 
@@ -40,6 +41,37 @@ export default class ProductManager {
       return;
     }
 
+    // Validar el tipo de dato de los campos
+    if (typeof product.title !== "string") {
+      console.log("El campo 'title' debe ser una cadena de caracteres");
+      return;
+    }
+
+    if (typeof product.description !== "string") {
+      console.log("El campo 'description' debe ser una cadena de caracteres");
+      return;
+    }
+
+    if (typeof product.category !== "string") {
+      console.log("El campo 'category' debe ser una cadena de caracteres");
+      return;
+    }
+
+    if (typeof product.price !== "number") {
+      console.log("El campo 'price' debe ser un número");
+      return;
+    }
+
+    if (typeof product.code !== "string") {
+      console.log("El campo 'code' debe ser una cadena de caracteres");
+      return;
+    }
+
+    if (typeof product.stock !== "number") {
+      console.log("El campo 'stock' debe ser un número");
+      return;
+    }
+
     // Validar que no se repita el código
     const codeAlreadyExists = this.products.some(
       (prod) => prod.code === product.code
@@ -50,11 +82,24 @@ export default class ProductManager {
       return;
     }
 
+    // Validar y establecer el valor por defecto para el campo status
+    const status = typeof product.status === "boolean" ? product.status : true;
+
+    // Validar y obtener las rutas completas de las imágenes (thumbnails)
+    const thumbnails = Array.isArray(product.thumbnails)
+      ? product.thumbnails
+      : [];
+
+    const thumbnailsWithFullPath = thumbnails.map(
+      (thumbnail) => this.thumbnailsPath + thumbnail
+    );
+
     // Agregar el producto al arreglo con un id autoincrementable
     const newProduct = {
       ...product,
       id: this.products.length + 1,
-      status: true
+      status,
+      thumbnails: thumbnailsWithFullPath, // Agregar las rutas completas de las imágenes
     };
 
     this.products.push(newProduct);
@@ -70,7 +115,7 @@ export default class ProductManager {
     const product = this.products.find((product) => product.id === id);
     if (product) {
       return product;
-    } 
+    }
   }
 
   async updateProduct(id, updatedProduct) {
